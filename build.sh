2caa991bc2782d48c7de9b4e6cd5f046dafeb8cf
Jakub Dobes <dobes.jakub@gmail.com>
2024-02-19 14:01:15 -0800

    Set up vita toolchain

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+build_type=Debug
+
+usage() {
+    echo "Usage: $0 [-r -h]"
+    echo
+    echo "  -r : Build release"
+    echo "  -h : Display this message"
+}
+
+while getopts 'rh' flag; do
+    case "${flag}" in
+        r) build_type=Release ;;
+        h) usage
+        exit 1 ;;
+    esac
+done
+
+rm -rf build
+cmake -S . -B build -DCMAKE_BUILD_TYPE=$build_type
+cmake --build build
